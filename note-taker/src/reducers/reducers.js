1ef8dffd89a5536b44b9f292d6ca7e6106db460b
Your Name <you@example.com>
2019-11-04 17:41:12 -0600

    added action and reducer,reducers.js

--- note-taker/src/reducers/reducers.js
+++ note-taker/src/reducers/reducers.js
@@ -1,8 +1,27 @@
+import {ADD_NOTE} from '../actions/actions'
+
+const initialState={
+        notes:[]
+};
 
 
-export const rootReducer=(state,initialSate,action)=>{
-        return state;
+function rootReducer(state=initialState, action){
+        switch(action.type){
+                case ADD_NOTE:
+                        return{
+                                notes:[
+                                        ...state.notes,
+                                        {
+                                                title:action.title,
+                                                content:action.content
+
+                                        }
+                                ]
+                        };
+                        default:
+                                return state;
+
+        };
 }
 
 export default rootReducer;
-//sfdsdf
